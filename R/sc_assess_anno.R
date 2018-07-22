2b2cc41c7c52c16fddbda658337854a5f046800a
yuqiyuqitan <tanyuqi527@gmail.com>
2018-07-22 19:34:31 -0400

    assessment plotting updates

--- R/sc_assess_anno.R
+++ R/sc_assess_anno.R
@@ -776,17 +776,30 @@ plot_class_ROCs<-function
 }
 
 plot_multiAssess <- function(assessed, method = "tsp_rf"){
-  metric <- matrix(0, ncol = 1, nrow = 3)
-  rownames(metric) <- c("cohen's kappa", "accuracy", "multiLogLoss")
-  colnames(metric) <- method
-  metric[1:3,] <- c(assessed$kappa, assessed$accuracy, assessed$multiLogLoss) 
+  #metric <- matrix(0, ncol = 3, nrow = 1)
+  #colnames(metric) <- c("cohen's kappa", "accuracy", "multiLogLoss")
+  #colnames(metric) <- method
+  #metric[,1:3] <- c(assessed$kappa, assessed$accuracy, assessed$multiLogLoss) 
+  #metric <- as.data.frame(metric)
+  #test <- melt(data = t(metric))
+  #p1 <- ggplot(test, aes(x=Var2, y= value, fill = Var2)) + geom_bar(stat="identity") + scale_fill_brewer(palette="Set2") +xlab("") + ylab("") + theme(axis.text=element_text(size=8), axis.title=element_text(size=8)) + labs(fill = method) + ylim(0,1)
+  metric <- matrix(0, ncol = 3, nrow = 1)
+  colnames(metric) <- c("cohen's kappa", "accuracy", "multiLogLoss")
+  rownames(metric) <- "value"
+  metric[,1:3] <- c(tm_heldoutassessment$kappa, tm_heldoutassessment$accuracy, tm_heldoutassessment$multiLogLoss) 
   metric <- as.data.frame(metric)
-  test <- melt(data = t(metric))
- 
-  p1 <- ggplot(test, aes(x=Var2, y= value, fill = Var2)) + geom_bar(stat="identity") + scale_fill_brewer(palette="Set2") +xlab("") + ylab("") + theme(axis.text=element_text(size=8), axis.title=element_text(size=8)) + labs(fill = method) + ylim(0,1)
-  p2 <- ggplot(data=assessed$PR_ROC, aes(x=as.numeric(as.vector(recall)), y=as.numeric(as.vector(precision)))) + geom_point(size = .5, alpha=.5) +  geom_path(size=.5, alpha=.75) +
+
+  p1<-ggplot(metric, aes(x="cohen's kappa", y = metric[1,1])) + geom_bar(stat="identity") +xlab("") + ylab("") + theme(axis.text=element_text(size=8), axis.title=element_text(size=8)) +  ylim(0,1) + theme(legend.position="none")
+
+  p2<-ggplot(metric, aes(x="ccuracy", y = metric[1,2])) + geom_bar(stat="identity") +xlab("") + ylab("") + theme(axis.text=element_text(size=8), axis.title=element_text(size=8)) + ylim(0,1) + theme(legend.position="none")
+
+  p3<-ggplot(metric, aes(x="multiLogLoss", y = metric[1,3])) + geom_bar(stat="identity") +xlab("") + ylab("") + theme(axis.text=element_text(size=8), axis.title=element_text(size=8)) + ylim(0,25)+ theme(legend.position="none")
+
+  p4 <- ggplot(data=assessed$PR_ROC, aes(x=as.numeric(as.vector(recall)), y=as.numeric(as.vector(precision)))) + geom_point(size = .5, alpha=.5) +  geom_path(size=.5, alpha=.75) +
     theme_bw() + xlab("Recall") + ylab("Precision") + facet_wrap( ~ ctype, ncol=4) +
     theme(axis.text = element_text(size=5)) + ggtitle("Classification performance_PR Curve")
- 
-  p2 + p1 + plot_layout(ncol = 2)
+
+  (p1 | p2 | p3) /
+      p4
+
 }
