a843fb267801ee15a62db8e58609081dbf7e4125
pcahan1 <patrick.cahan@gmail.com>
2018-11-02 14:05:23 -0400

    themes, umap

--- R/plots.R
+++ R/plots.R
@@ -3,8 +3,6 @@
 
 
 
-
-
 #' @export
 prep_umap_class<-function
 (classRes,
@@ -1984,52 +1982,76 @@ plot_bundle<-function(gpaRes, bundleRes, legend=TRUE)
   ans
 }
 
+# theme_dviz_hgrid  from https://github.com/clauswilke/dataviz/blob/master/R/themes.R
+#' @export
+theme_dviz_hgrid <- function(font_size = 14, font_family = "") { 
+  color = "grey90"
+  line_size = 0.5
+
+  # Starts with theme_cowplot and then modify some parts
+  theme_cowplot(font_size = font_size, font_family = font_family) %+replace% 
+  theme( 
+    # make horizontal grid lines
+    panel.grid.major = element_line(colour = color, 
+    size = line_size), 
+    panel.grid.major.x = element_blank(), 
+    
+    # adjust axis tickmarks
+    axis.ticks = element_line(colour = color, size = line_size), 
+
+    # adjust x axis
+    axis.line.x = element_line(colour = color, size = line_size), 
+
+    # no y axis line
+    axis.line.y = element_blank() ) 
+} 
 
 
 # theme_black from https://gist.github.com/jslefche/eff85ef06b4705e6efbc
+#' @export
 theme_black = function(base_size = 12, base_family = "") { 
   library(gridExtra) 
-theme_grey(base_size = base_size, base_family = base_family) %+replace% 
-theme( 
-# Specify axis options
-axis.line = element_blank(), 
-axis.text.x = element_text(size = base_size*0.8, color = "white", lineheight = 0.9), 
-axis.text.y = element_text(size = base_size*0.8, color = "white", lineheight = 0.9), 
-axis.ticks = element_line(color = "white", size = 0.2), 
-#axis.title.x = element_text(size = base_size, color = "white", margin = margin(0, 10, 0, 0)), 
-#axis.title.y = element_text(size = base_size, color = "white", angle = 90, margin = margin(0, 10, 0, 0)), 
-axis.ticks.length = unit(0.3, "lines"), 
-# Specify legend options
-legend.background = element_rect(color = NA, fill = "black"), 
-legend.key = element_rect(color = "white", fill = "black"), 
-legend.key.size = unit(1.2, "lines"), 
-legend.key.height = NULL, 
-legend.key.width = NULL, 
-legend.text = element_text(size = base_size*0.8, color = "white"), 
-legend.title = element_text(size = base_size*0.8, face = "bold", hjust = 0, color = "white"), 
-legend.position = "right", 
-legend.text.align = NULL, 
-legend.title.align = NULL, 
-legend.direction = "vertical", 
-legend.box = NULL, 
-# Specify panel options
-panel.background = element_rect(fill = "black", color = NA), 
-panel.border = element_rect(fill = NA, color = "white"), 
-panel.grid.major = element_line(color = "grey35"), 
-panel.grid.minor = element_line(color = "grey20"), 
-#panel.margin = unit(0.5, "lines"), 
-# Specify facetting options
-###strip.background = element_rect(fill = "grey30", color = "grey10"), 
-strip.background = element_blank(),
-###strip.text.x = element_text(size = base_size*0.8, color = "white"), 
-strip.text.x = element_text(size = base_size, color = "white"), 
-strip.text.y = element_text(size = base_size*0.8, color = "white",angle = -90), 
-# Specify plot options
-plot.background = element_rect(color = "black", fill = "black"), 
-###plot.title = element_text(size = base_size*1.2, color = "white"), 
-plot.title = element_text(size = base_size, color = "white"), 
-##plot.margin = unit(rep(1, 4), "lines") 
-) 
+  theme_grey(base_size = base_size, base_family = base_family) %+replace% 
+  theme( 
+    # Specify axis options
+    axis.line = element_blank(), 
+    axis.text.x = element_text(size = base_size*0.8, color = "white", lineheight = 0.9), 
+    axis.text.y = element_text(size = base_size*0.8, color = "white", lineheight = 0.9), 
+    axis.ticks = element_line(color = "white", size = 0.2), 
+    #axis.title.x = element_text(size = base_size, color = "white", margin = margin(0, 10, 0, 0)), 
+    #axis.title.y = element_text(size = base_size, color = "white", angle = 90, margin = margin(0, 10, 0, 0)), 
+    axis.ticks.length = unit(0.3, "lines"), 
+    # Specify legend options
+    legend.background = element_rect(color = NA, fill = "black"), 
+    legend.key = element_rect(color = "white", fill = "black"), 
+    legend.key.size = unit(1.2, "lines"), 
+    legend.key.height = NULL, 
+    legend.key.width = NULL, 
+    legend.text = element_text(size = base_size*0.8, color = "white"), 
+    legend.title = element_text(size = base_size*0.8, face = "bold", hjust = 0, color = "white"), 
+    legend.position = "right", 
+    legend.text.align = NULL, 
+    legend.title.align = NULL, 
+    legend.direction = "vertical", 
+    legend.box = NULL, 
+    # Specify panel options
+    panel.background = element_rect(fill = "black", color = NA), 
+    panel.border = element_rect(fill = NA, color = "white"), 
+    panel.grid.major = element_line(color = "grey35"), 
+    panel.grid.minor = element_line(color = "grey20"), 
+    #panel.margin = unit(0.5, "lines"), 
+    # Specify facetting options
+    ###strip.background = element_rect(fill = "grey30", color = "grey10"), 
+    strip.background = element_blank(),
+    ###strip.text.x = element_text(size = base_size*0.8, color = "white"), 
+    strip.text.x = element_text(size = base_size, color = "white"), 
+    strip.text.y = element_text(size = base_size*0.8, color = "white",angle = -90), 
+    # Specify plot options
+    plot.background = element_rect(color = "black", fill = "black"), 
+    ###plot.title = element_text(size = base_size*1.2, color = "white"), 
+    plot.title = element_text(size = base_size, color = "white"), 
+    ##plot.margin = unit(rep(1, 4), "lines")
+  ) 
 }
 
 
