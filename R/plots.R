d7a0ac11b4ce2d4c37e4edf6e8987031f85ae69c
yuqiyuqitan <tanyuqi527@gmail.com>
2018-12-28 16:13:26 -0500

    readme plots updated and sc_violin xaxis-angle =45c

--- R/plots.R
+++ R/plots.R
@@ -45,7 +45,7 @@ ggplot(test, aes(x = cluster, y = classification_score, fill = cluster)) + ylim(
      axis.ticks.x = element_blank(), 
      axis.title.y = element_text(size = 8),
     axis.title.x = element_text(size = 8),
-    axis.text.x = element_text(size = 8),
+    axis.text.x = element_text(size = 8, angle = 45, hjust=1),
     axis.text.y = element_text(size = 8),
     legend.title=element_text(size=10), 
     legend.text=element_text(size=10),
